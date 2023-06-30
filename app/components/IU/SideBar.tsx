d585f5c1854130166609065d93d5f1f109befd6b
Garber Batista <garbfbatista@gmail.com>
2023-06-30 16:13:14 -0400

    Creacion de sidebar

--- app/components/IU/SideBar.tsx
+++ app/components/IU/SideBar.tsx
@@ -98,18 +98,7 @@ export const SideBar: FC<PropsWithChildren<Props>> = ({ children }) => {
                     <path d='M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z'></path>
                   </svg>
                 </svg>
-                <span className='px-2'>Clientes</span>
-              </button>
-              <button className='flex justify-start items-center text-sm font-medium text-gray-700 py-2 px-2 hover:bg-teal-500 hover:text-white rounded-md transition duration-150 ease-in-out'>
-                <svg
-                  className='w-6 h-6 fill-current inline-block'
-                  fill='currentColor'
-                  viewBox='0 0 20 20'
-                  xmlns='http://www.w3.org/2000/svg'
-                >
-                  <path d='M13 6a3 3 0 11-6 0 3 3 0 016 0zM18 8a2 2 0 11-4 0 2 2 0 014 0zM14 15a4 4 0 00-8 0v3h8v-3zM6 8a2 2 0 11-4 0 2 2 0 014 0zM16 18v-3a5.972 5.972 0 00-.75-2.906A3.005 3.005 0 0119 15v3h-3zM4.75 12.094A5.973 5.973 0 004 15v3H1v-3a3 3 0 013.75-2.906z'></path>
-                </svg>
-                <span className='px-2'>Proveedores</span>
+                <span className='px-2'>Condutor</span>
               </button>
             </div>
           </div>
